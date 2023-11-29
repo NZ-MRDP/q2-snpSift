cc215f0c5bef3c18227363c52d39f40c1915de5e
hnko <henzikou@gmail.com>
2023-11-29 11:50:46 -0800

    update poetry dependencies

---
+++ q2_snpsift/_type.py
@@ -0,0 +1,12 @@
+# TODO: Delete after Megan has merged her branch into main
+
+from q2_types.feature_data import FeatureData
+from qiime2.plugin import SemanticType
+
+VCFFormat = SemanticType("VCFFormat", variant_of=FeatureData.field["type"])
+
+DictFormat = SemanticType("DictFormat", variant_of=FeatureData.field["type"])
+
+MetricsFormat = SemanticType("MetricsFormat", variant_of=FeatureData.field["type"])
+
+BamIndexFormat = SemanticType("BamIndexFormat", variant_of=FeatureData.field["type"])
