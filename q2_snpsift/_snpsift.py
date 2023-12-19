973f83982f9b257c3c04acd6c90eff5bf240d1d4
hnko <henzikou@gmail.com>
2023-12-19 13:59:56 -0800

    add snpSift functions

---
+++ q2_snpsift/_snpsift.py
@@ -0,0 +1,52 @@
+"""SnpSift functions."""
+import os
+import subprocess
+from importlib import resources
+
+from q2_snpsift import bin
+
+from ._format import VariantAnnotationDirFormat, VariantDirFormat
+
+
+def filter(
+    input_vcf: VariantDirFormat,
+    expression: str,
+) -> VariantDirFormat:
+    """
+    Filter variants based on specific expression criteria.
+
+    Arguments:
+        input_vcf -- VariantDirFormat
+        expression -- str
+
+    Returns:
+        VariantDirFormat
+    """
+    filtered_vcf = VariantDirFormat()
+
+    with resources.path(bin, "SnpSift.jar") as executable_path:
+        cmd = [
+            "java",
+            "-jar",
+            executable_path,
+            "filter",
+            expression,
+            "-f",
+            os.path.abspath(os.path.join(str(input_vcf), "vcf.vcf")),
+        ]
+        subprocess.run(cmd, check=True, stdout=open(os.path.join(str(filtered_vcf), "vcf.vcf"), "w"))
+
+    return filtered_vcf
+
+
+def extract_fields_from_snpeff_output(vcf_file: VariantAnnotationDirFormat) -> VariantAnnotationDirFormat:
+    """
+    Extract fields from a VCF file to a txt, tab separated format, file.
+
+    Arguments:
+        vcf_file -- VariantAnnotationDirFormat
+
+    Returns:
+        VariantAnnotationDirFormat
+    """
+    return vcf_file
---
+++ q2_snpsift/_type.py
@@ -0,0 +1,12 @@
+from q2_types.feature_data import FeatureData
+from qiime2.plugin import SemanticType
+
+VariantType = SemanticType("VariantType", variant_of=FeatureData.field["type"])
+
+VariantAnnotationType = SemanticType("VariantAnnotationType", variant_of=FeatureData.field["type"])
+
+DictFormat = SemanticType("DictFormat", variant_of=FeatureData.field["type"])
+
+MetricsFormat = SemanticType("MetricsFormat", variant_of=FeatureData.field["type"])
+
+BamIndexFormat = SemanticType("BamIndexFormat", variant_of=FeatureData.field["type"])
