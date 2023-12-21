f2a56d60830d52fe88a01f6e73bbe4c062adf559
johnchase <johnhchase@protonmail.com>
2023-12-20 19:44:55 -0800

    Updates version

--- q2_snpsift/__init__.py
+++ q2_snpsift/__init__.py
@@ -1,6 +1,6 @@
 """snpSift python library."""
 from ._snpsift import extract_fields_from_snpeff_output, filter
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ["filter", "extract_fields_from_snpeff_output"]
